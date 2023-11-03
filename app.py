8de9544a65be7ffa5f1bfc9551e91ab155bdc2b0
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-03 15:31:51 +0900

    changed port

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def psychological_test():
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0", port=3000)
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3001)
